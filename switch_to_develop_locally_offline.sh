7cd80ec66d725f33d1b827d21de16138cbbebdb5
cesine <cesine@yahoo.com>
2013-02-05 13:43:49 +0000

    removing cruft

--- switch_to_develop_locally_offline.sh
+++ switch_to_develop_locally_offline.sh
@@ -5,15 +5,6 @@ echo "Put the Node server to act as the localhost server"
 sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' service.js  > output
 mv output service.js
 
-sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' lib/userauthentication.js  > output
-mv output lib/userauthentication.js
-sed 's/couchkeys_[^)]*)/couchkeys_local")/' lib/userauthentication.js  > output
-mv output lib/userauthentication.js
-sed 's/mailconfig_[^)]*)/mailconfig_local")/' lib/userauthentication.js  > output
-mv output lib/userauthentication.js
-
-sed 's/couchkeys_[^)]*)/couchkeys_local")/' lib/corpusmanagement.js  > output
-mv output lib/corpusmanagement.js
 
 echo "Now running in local/offline developer mode using the localhost."
 
