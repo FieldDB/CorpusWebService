3c2b6f2383f93cafedbec0aee51af7c8a6c2a9ca
cesine <cesine@yahoo.com>
2013-02-05 11:26:41 -0500

    bin was being ignored for this node project (unnessarily) adding in the onlycoucdb service which hardcodes the proxy to only work with a couchdb service on the localhost at port 5984 for https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/838

---
+++ bin/onlycouchdb.js
@@ -0,0 +1,13 @@
+//#!/usr/bin/env node
+var corsproxy = require("../lib/corsproxy");
+var httpProxy = require("http-proxy");
+/* Edited README to show that the target is not supposed to be an object, but rather a well formed url for a couchdb etc */
+corsproxy.options = {
+    target: "http://127.0.0.1:5984"
+};
+var host = process.env.HOST || process.argv[2] || "127.0.0.1";
+var port = process.env.PORT || process.argv[3] || 9292;
+
+httpProxy.createServer(corsproxy).listen(port, host, function() {
+  console.log("CORS Proxy started on %s:%d", host, port);
+});
