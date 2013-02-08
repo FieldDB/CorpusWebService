9f344dea59935722df6a40d85e946765345f801b
cesine <cesine@yahoo.com>
2013-02-08 00:09:08 +0000

    worked on https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/838 tested the couchapp, all seems to behave normally as if the cors proxy wasnt there (this is to be exected, the cors proxy shouldnt be in effect given that the domains match.

--- switch_to_develop_easy_setup.sh
+++ switch_to_develop_easy_setup.sh
@@ -2,8 +2,8 @@
 
 echo ""
 echo "Put the Node server to act as the development server"
-sed 's/nodeconfig_[^)]*)/nodeconfig_devserver")/' service.js  > output
-mv output service.js
+sed 's/nodeconfig_[^)]*)/nodeconfig_devserver")/' lib/corsproxy.js  > output
+mv output lib/corsproxy.js
 
 
 echo "Now running in developer mode."
--- switch_to_develop_locally_offline.sh
+++ switch_to_develop_locally_offline.sh
@@ -2,8 +2,8 @@
 
 echo ""
 echo "Put the Node server to act as the localhost server"
-sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' service.js  > output
-mv output service.js
+sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' lib/corsproxy.js  > output
+mv output lib/corsproxy.js
 
 
 echo "Now running in local/offline developer mode using the localhost."
